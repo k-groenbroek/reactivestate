dcd1435532ec46ce3829ab4a4e7a3563544cfb90
K. Groenbroek <kasper.groenbroek@ortec.com>
2022-02-28 22:50:30 +0100

    Move mdx compiler.

--- src/appup/core/mdx.py
+++ src/appup/core/mdx.py
@@ -7,15 +7,14 @@ def compile(filepath):
     Compile mdx file to Javascript and return content as string.
     """
     args = [
-        os.path.abspath("src/bin/qjs.exe"),
+        os.path.abspath("assets/qjs.exe"),
         "--module",
-        os.path.abspath("src/bin/mdxcompiler.js"),
+        os.path.abspath("assets/mdxcompiler.js"),
         os.path.abspath(filepath),
     ]
     p = subprocess.Popen(
         args,
         stdout=subprocess.PIPE,
-        cwd="src/bin",
         shell=False,
         encoding="UTF-8",
         close_fds=False,
